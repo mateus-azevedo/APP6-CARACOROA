6d5d44b86004ecce5adde65ba12c349f19166a5e
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-23 18:21:11 -0300

    criado cena resultado e navegacao

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ import {Router, Scene, Stack} from 'react-native-router-flux';
 import Principal from './src/components/Principal';
 import SobreJogo from './src/components/SobreJogo';
 import OutrosJogos from './src/components/OutrosJogos';
+import Resultado from './src/components/Resultado';
 
 export default class app6 extends Component {
   render() {
     return(
-      <Router sceneStyle={{ paddingTop: 0 }}>
+      <Router>
         <Stack key='root'>
           <Scene key='principal' component={Principal} initial title='Cara ou Coroa' />
           <Scene key='sobrejogo' component={SobreJogo} title='Sobre o Jogo' />
           <Scene key='outrosjogos' component={OutrosJogos} title='Outros Jogos' />
+          <Scene key='resultado' component={Resultado} title='Resultado' />
         </Stack>
       </Router>
     );
