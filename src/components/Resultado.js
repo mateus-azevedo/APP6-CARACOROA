488d280d861cc2ecb802c8e71fd4ba5b881aab58
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-23 18:33:50 -0300

    criado logica cara ou coroa

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -5,13 +5,39 @@ const cara = require('../imgs/moeda_cara.png');
 const coroa = require('../imgs/moeda_coroa.png');
 
 export default class Resultado extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {resultado: ''};
+  }
+
+  UNSAFE_componentWillMount() {
+    const numeroAleatorio = Math.floor(Math.random() * 2);
+    let caraOuCoroa = '';
+
+    if (numeroAleatorio === 0) {
+      caraOuCoroa = 'cara';
+    } else {
+      caraOuCoroa = 'coroa';
+    }
+
+    this.setState({resultado: caraOuCoroa});
+  }
+  
   render() {
-    return(
-      <View style={styles.resultado}>
-        <Image source={cara} />
-        <Image source={coroa} />
-      </View>
-    );
+    if (this.state.resultado === 'cara') {
+      return(
+        <View style={styles.resultado}>
+          <Image source={cara} />
+        </View>
+      );
+    } else {
+      return(
+        <View style={styles.resultado}>
+          <Image source={coroa} />
+        </View>
+      );
+    }
   }
 }
 
