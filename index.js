d7ac24c1c5e75991d6a23942969bd44c45f9d2bd
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-23 00:15:51 -0300

    iniciando React Native Router Flux

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import {Router, Scene} from 'react-native-router-flux';
+
+import Principal from './src/components/Principal';
+import SobreJogo from './src/components/SobreJogo';
+import OutrosJogos from './src/components/OutrosJogos';
 
 export default class app6 extends Component {
   render() {
-    return();
+    return(
+      <Router>
+        <Scene key='principal' component={Principal} initil title='Cara ou Coroa' />
+        <Scene key='sobrejogo' component={SobreJogo} />
+        <Scene key='outrosjogos' component={OutrosJogos} />
+      </Router>
+    );
   }
 }
 
