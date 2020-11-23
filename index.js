a66b57cf0a7c8c7da48e84b905569de0d9323648
Mateus Santos <62727807+mateus-azevedo@users.noreply.github.com>
2020-11-23 17:55:08 -0300

    Issue Resolved (#1) React Native Router Flux
    * installed RN gesture-handler, reanimated, screens
    
    * utilizando Stack
    
    * corrigido atributo initial

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import {Router, Scene} from 'react-native-router-flux';
+import {Router, Scene, Stack} from 'react-native-router-flux';
 
 import Principal from './src/components/Principal';
 import SobreJogo from './src/components/SobreJogo';
@@ -10,10 +10,12 @@ import OutrosJogos from './src/components/OutrosJogos';
 export default class app6 extends Component {
   render() {
     return(
-      <Router sceneStyle={{ paddingTop: 50 }}>
-        <Scene key='principal' component={Principal} initil title='Cara ou Coroa' />
-        <Scene key='sobrejogo' component={SobreJogo} title='Sobre o Jogo' />
-        <Scene key='outrosjogos' component={OutrosJogos} title='Outros Jogos' />
+      <Router sceneStyle={{ paddingTop: 0 }}>
+        <Stack key='root'>
+          <Scene key='principal' component={Principal} initial title='Cara ou Coroa' />
+          <Scene key='sobrejogo' component={SobreJogo} title='Sobre o Jogo' />
+          <Scene key='outrosjogos' component={OutrosJogos} title='Outros Jogos' />
+        </Stack>
       </Router>
     );
   }
