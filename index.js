b8aff214ad6d27ca5a47f350ab28db0eade97e32
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-23 18:47:23 -0300

    refact e rotas viraram componente

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import {Router, Scene, Stack} from 'react-native-router-flux';
 
-import Principal from './src/components/Principal';
-import SobreJogo from './src/components/SobreJogo';
-import OutrosJogos from './src/components/OutrosJogos';
-import Resultado from './src/components/Resultado';
+import Rotas from './src/Rotas';
 
 export default class app6 extends Component {
   render() {
     return(
-      <Router>
-        <Stack key='root'>
-          <Scene key='principal' component={Principal} initial title='Cara ou Coroa' />
-          <Scene key='sobrejogo' component={SobreJogo} title='Sobre o Jogo' />
-          <Scene key='outrosjogos' component={OutrosJogos} title='Outros Jogos' />
-          <Scene key='resultado' component={Resultado} title='Resultado' />
-        </Stack>
-      </Router>
+      <Rotas />
     );
   }
 }
--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Image, View, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { Image, View, StyleSheet } from 'react-native';
 
 const cara = require('../imgs/moeda_cara.png');
 const coroa = require('../imgs/moeda_coroa.png');
@@ -8,7 +8,7 @@ export default class Resultado extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {resultado: ''};
+    this.state = { resultado: '' };
   }
 
   UNSAFE_componentWillMount() {
@@ -21,23 +21,23 @@ export default class Resultado extends Component {
       caraOuCoroa = 'coroa';
     }
 
-    this.setState({resultado: caraOuCoroa});
+    this.setState({ resultado: caraOuCoroa });
   }
-  
+
   render() {
     if (this.state.resultado === 'cara') {
-      return(
+      return (
         <View style={styles.resultado}>
           <Image source={cara} />
         </View>
       );
-    } else {
-      return(
-        <View style={styles.resultado}>
-          <Image source={coroa} />
-        </View>
-      );
     }
+    
+    return (
+      <View style={styles.resultado}>
+        <Image source={coroa} />
+      </View>
+    );
   }
 }
 
@@ -47,5 +47,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#61BD8C',
     justifyContent: 'center',
     alignItems: 'center'
-  }
-})
+  },
+});
